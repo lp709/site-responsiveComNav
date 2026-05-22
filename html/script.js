cb4b8c1fa5041778f2d8037bb892ab8515136d3f
Lp <pineiroleandro7009@gmail.com>
2026-05-22 21:00:03 +0100

    feat: corigindo a nav e adicionando mais recursos no projecto, adicionei 1 butao hover

---
+++ html/script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('a')[4].onclick = function lp() {
+    const curso = 'Curso Não Desponivel';
+    window.alert(curso);
+}
+
+
+/*function lp(a) {
+    const curso = 'Curso Não Disponivel';
+    window.alert(curso);
+}
+document.querySelectorAll('a')[4].onclick = lp;*/
