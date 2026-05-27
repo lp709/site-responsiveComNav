a13aa9fa1a5d85726be5bcbb00cd361175b6ab7f
Lp <pineiroleandro7009@gmail.com>
2026-05-27 07:16:04 +0100

    adicionei um hr e estilizei com css

--- html/script.js
+++ html/script.js
@@ -1,28 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
 const TodosOsLinks = document.querySelectorAll('a');
 TodosOsLinks.forEach(function (links) {
     links.onclick = function () {
-        const curso = 'Este Curso ou Esta Funcionalidade Não Estão Disponiveis';
+        const curso = 'Este Curso ou Esta Funcionalidade Não Estão Disponiveis No Momento, Obrigado!';
         window.alert(curso);
     }
 });
-
-function liberarAudio() {
     const botao = document.querySelector('#btn-musica');
     const audio = document.querySelector('#meu-audio');
 
-    botao.style.display = 'none';
-
-    audio.style.display = 'flex';
-
-
-    audio.play();
-
-
-    audio.addEventListener('play', function () {
-        console.log('Playing Song: ' + !false);
+    botao.addEventListener('click', function() {
+        botao.classList.add('escondido');
+        audio.style.display = 'block';
+        audio.play();
     });
 
-    audio.addEventListener('pause', function () {
-        console.log('Playing Song: ' + !true);
+    audio.addEventListener('ended', function() {
+        botao.classList.remove('escondido');
+        audio.style.display = 'none';
     });
-};
+});
